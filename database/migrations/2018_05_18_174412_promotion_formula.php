cc99d4e608315bdb67be8a8354dd4f6fa50db426
arvind-verz <arvind-verz@users.noreply.github.com>
2018-05-18 15:22:48 +0530

    updates on 18-05

--- database/migrations/2018_05_18_174412_promotion_formula.php
+++ database/migrations/2018_05_18_174412_promotion_formula.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMenusTable extends Migration
+class PromotionFormula extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,10 @@ class CreateMenusTable extends Migration
      */
     public function up()
     {
-        Schema::create('menus', function (Blueprint $table) {
+        Schema::create('promotion_formula', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title')->nullable();
-            $table->integer('main')->nullable();
-            $table->integer('parent')->nullable();
-            $table->integer('child')->nullable();
-            $table->tinyInteger('delete_status')->default("0");
+            $table->integer('promotion_id');
+            $table->string('name');
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ class CreateMenusTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('menus');
+        //
     }
 }
