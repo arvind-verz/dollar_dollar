3a27206b770b5bd319e9ecba09e549ed9899e8c6
nicckk3 <nicckk3@gmail.com>
2018-06-06 12:50:50 +0530

    add condition in getBackendmenus

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -349,6 +349,7 @@ class Helper
         $checkMenuPermission = \DB::table('role_type_access')
             ->join('modules', 'role_type_access.module_id', '=', 'modules.id')
             ->where(['role_type_access.role_type_id' => @Auth::user()->role_type_id, 'role_type_access.view' => 1])
+            ->where('modules.delete_status',0)
             ->select('role_type_access.module_id', 'modules.name', 'modules.label', 'modules.icon')
             ->orderBy('modules.view_order', 'asc')
             ->get();
