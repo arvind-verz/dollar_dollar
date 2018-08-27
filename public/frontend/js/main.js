a780dcfe83c042b4299e7e878babbfce31fd03dc
arvind-verz <arvind-verz@users.noreply.github.com>
2018-08-27 09:39:31 +0530

    updates

--- public/frontend/js/main.js
+++ public/frontend/js/main.js
@@ -342,3 +342,13 @@ $(window).on('load resize', function() {
         $(".ps-home--links a").eq(n).addClass("active");
     });
 });
+
+$(document).ready(function() {
+    $(".ps-checkbox input[type=checkbox]:checked").parent().parent().addClass("active");
+    
+    $("body").on("click", ".combine-criteria-padding .ps-checkbox label",function(){
+        setTimeout(function(){$(".combine-criteria-padding .ps-checkbox input[type=checkbox]:checked").each(function(){
+                $(this).parent().parent().addClass("active");
+        });},100);
+    });
+});
