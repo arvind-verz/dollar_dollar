d1df7796ef1405168abec6c1d0c183591c499059
arvind-verz <arvind-verz@users.noreply.github.com>
2018-09-10 12:14:29 +0530

    updates

--- app/Http/Controllers/Reports/ReportController.php
+++ app/Http/Controllers/Reports/ReportController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Reports;
 
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\ProductManagement;
 
@@ -19,21 +18,29 @@ class ReportController extends Controller
      */
     public function customer_report()
     {
-        $customer_reports   =   ProductManagement::join('users', 'product_managements.user_id', '=', 'users.id')
-        ->join('brands', 'product_managements.bank_id', '=', 'brands.id')
-        ->get();
+        $customer_reports = ProductManagement::join('users', 'product_managements.user_id', '=', 'users.id')
+            ->join('brands', 'product_managements.bank_id', '=', 'brands.id')
+            ->select('users.id as users_id', 'users.*', 'brands.*', 'product_managements.*')
+            ->get();
 
+        $customer_reports_groups = ProductManagement::join('users', 'product_managements.user_id', '=', 'users.id')
+            ->join('brands', 'product_managements.bank_id', '=', 'brands.id')
+            ->groupBy('users.id')
+            ->select('users.id as users_id', 'users.*', 'brands.*', 'product_managements.*')
+            ->get();
+//dd($customer_reports);
         return view('backend.reports.customer-report.index', [
-            'customer_reports'      =>  $customer_reports
+            'customer_reports'  =>  $customer_reports,
+            'customer_reports_groups' => $customer_reports_groups,
         ]);
     }
 
     public function product_report()
     {
-        $product_reports   =   ProductManagement::join('brands', 'product_managements.bank_id', '=', 'brands.id')
-        ->get();
+        $product_reports = ProductManagement::join('brands', 'product_managements.bank_id', '=', 'brands.id')
+            ->get();
         return view('backend.reports.product-report.index', [
-            'product_reports'      =>  $product_reports
+            'product_reports' => $product_reports,
         ]);
     }
 }
