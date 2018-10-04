11955b7d510ae49c6ecb8ac98877aa16d23b9737
nicckk3 <nicckk3@gmail.com>
2018-10-04 15:29:55 +0530

    commit

--- resources/views/frontend/htmlLayouts/aioMoreDetailF2.blade.php
+++ resources/views/frontend/htmlLayouts/aioMoreDetailF2.blade.php
@@ -1,5 +1,6 @@
 <div class="ps-product__detail table" style="display: block;">
     <div class="ps-criteria-detail">
+        <p>This text only for test, Your text input here</p>
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
                 <h4 class="ps-product__heading">Criteria Details</h4>
@@ -26,6 +27,7 @@
                         </div>
                         <div class="ps-block--product-info">
                             <h5></h5>
+
                             <div class="ps-block__content">
                                 <p>Salary – Credit of Salary (min $2k)</p>
                                 <a class="ps-block__more content-detail" data-formula="2"
@@ -37,61 +39,54 @@
                 </div>
             </div>
         </div>
+
+        <div id="formula-2-salary" class="ps-criteria-detail__content">
+            <ul class="ps-list--arrow-circle">
+                <li>Salary Tab Section
+                </li>
+                <li>Only salary credit
+                    through giro with transaction description &lsquo;Giro &ndash;Salary&rsquo; in your statement will
+                    qualify.
+                </li>
+            </ul>
+        </div>
+        <div id="formula-2-giro" class="ps-criteria-detail__content">
+            <ul class="ps-list--arrow-circle">
+                <li>Payment Tab Section.
+                </li>
+                <li>Only salary credit
+                    through giro with transaction description &lsquo;Giro &ndash;Salary&rsquo; in your statement will
+                    qualify.
+                </li>
+            </ul>
+        </div>
+        <div id="formula-2-spend" class="ps-criteria-detail__content">
+            <ul class="ps-list--arrow-circle">
+                <li>Spend Tab Section.
+                </li>
+                <li>Only salary credit
+                    through giro with transaction description &lsquo;Giro &ndash;Salary&rsquo; in your statement will
+                    qualify.
+                </li>
+            </ul>
+        </div>
+        <div id="formula-2-details" class="ps-criteria-detail__content"></div>
     </div>
-    <div id="formula-2-salary" class="ps-criteria-detail__content">
-        <ul class="ps-list--arrow-circle">
-            <li>Salary Tab Section
-            </li>
-            <li>Only salary credit
-                through giro with transaction description &lsquo;Giro &ndash;Salary&rsquo; in your statement will
-                qualify.
-            </li>
-        </ul>
-    </div>
-    <div id="formula-2-giro" class="ps-criteria-detail__content">
-        <ul class="ps-list--arrow-circle">
-            <li>Payment Tab Section.
-            </li>
-            <li>Only salary credit
-                through giro with transaction description &lsquo;Giro &ndash;Salary&rsquo; in your statement will
-                qualify.
-            </li>
-        </ul>
-    </div>
-    <div id="formula-2-spend" class="ps-criteria-detail__content">
-        <ul class="ps-list--arrow-circle">
-            <li>Spend Tab Section.
-            </li>
-            <li>Only salary credit
-                through giro with transaction description &lsquo;Giro &ndash;Salary&rsquo; in your statement will
-                qualify.
-            </li>
-        </ul>
-    </div>
-    <div id="formula-2-details" class="ps-criteria-detail__content"></div>
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
-        <div class="row">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 ">
-                <h4 class="ps-product__heading">Criteria</h4>
-                <ul class="ps-list--arrow-circle">
-                    <li>Fresh funds #</li>
-                    <li>RHB Fixed Deposit account</li>
-                    <li>Placement done at Branch</li>
-                </ul>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+            <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
-                    <div class="ps-product__actions"><a class="ps-btn ps-btn--outline" target="_blank" href="#">Main Page</a><a class="ps-btn ps-btn--outline" href="#" target="_blank">T&amp;C</a></div>
+                    <h4 class="ps-product__heading">Keypoints</h4>
+                    <ul class="ps-list--arrow-circle">
+                        <li>Receive interest upfront</li>
+                        <li>Deposit into main account</li>
+                    </ul>
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+                        <div class="ps-product__actions"><a class="ps-btn ps-btn--outline" target="_blank" href="#">Main
+                                Page</a><a class="ps-btn ps-btn--outline" href="#" target="_blank">T&amp;C</a></div>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12 ">
-                <h4 class="ps-product__heading">Keypoints</h4>
-                <ul class="ps-list--arrow-circle">
-                    <li>Receive interest upfront</li>
-                    <li>Deposit into main account</li>
-                </ul>
             </div>
         </div>
     </div>
-</div>
 </div>
