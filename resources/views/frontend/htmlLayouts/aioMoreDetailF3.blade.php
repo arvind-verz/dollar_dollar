89c1654c62dc82f77a78ee59a6f51a3da0ac7ad8
nicckk3 <35618468+nicckk3@users.noreply.github.com>
2018-10-28 16:13:44 +0800

    commit

--- resources/views/frontend/htmlLayouts/aioMoreDetailF3.blade.php
+++ resources/views/frontend/htmlLayouts/aioMoreDetailF3.blade.php
@@ -10,56 +10,48 @@
                     <div class="table-new">
                         <div class="ps-block--product-info">
                             <h5>Salary</h5>
-
                             <div class="ps-block__content">
                                 <p>Credit Salary of $2k through giro</p>
                                 <a class="ps-block__more content-detail" data-formula="3" data-detail-id="formula-3-salary">SHOW DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Payment</h5>
-
                             <div class="ps-block__content">
                                 <p>Pay 3 bill online or through giro</p>
                                 <a class="ps-block__more content-detail" data-formula="3" data-detail-id="formula-3-payment">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Spend</h5>
-
                             <div class="ps-block__content">
                                 <p>Spend at least $500 on credit card</p>
                                 <a class="ps-block__more content-detail" data-formula="3" data-detail-id="formula-3-spend">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Hire purchase loan</h5>
-
                             <div class="ps-block__content">
                                 <p>Car loan of at least 50k</p>
                                 <a class="ps-block__more content-detail" data-formula="3" data-detail-id="formula-3-hire-purchase-loan">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Bonus</h5>
-
                             <div class="ps-block__content">
                                 <p>Earn additional 1% for extra balance</p>
                                 <a class="ps-block__more content-detail" data-formula="3" data-detail-id="formula-3-bonus">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Housing loan</h5>
-
                             <div class="ps-block__content">
                                 <p>Housing loan of at least100k</p>
                                 <a class="ps-block__more content-detail" data-formula="3" data-detail-id="formula-3-housing-loan">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Insurance</h5>
-
                             <div class="ps-block__content">
                      `           <p>Monthly insurance of $500</p>
                                 <a class="ps-block__more content-detail" data-formula="3" data-detail-id="formula-3-insurance">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Unit Trust</h5>
-
                             <div class="ps-block__content">
                                 <p>Total $30k of unit trust minimum</p>
                                 <a class="ps-block__more content-detail" data-formula="3" data-detail-id="formula-3-unit-trust">Show DETAILS</a></div>
@@ -69,6 +61,7 @@
             </div>
         </div>
         <div id="formula-3-salary" class="ps-criteria-detail__content">
+            <h5>Salary</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Salary Tab Section
                 </li>
@@ -79,6 +72,7 @@
             </ul>
         </div>
         <div id="formula-3-payment" class="ps-criteria-detail__content">
+            <h5>Payment</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Payment Tab Section.
                 </li>
@@ -89,6 +83,7 @@
             </ul>
         </div>
         <div id="formula-3-spend" class="ps-criteria-detail__content">
+            <h5>Spend</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Spend Tab Section.
                 </li>
@@ -99,6 +94,7 @@
             </ul>
         </div>
         <div id="formula-3-hire-purchase-loan" class="ps-criteria-detail__content">
+            <h5>Hire purchase loan</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Hire Purchase Loan Tab Section.
                 </li>
@@ -109,6 +105,7 @@
             </ul>
         </div>
         <div id="formula-3-bonus" class="ps-criteria-detail__content">
+            <h5>Bonus</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Bonus Tab Section.
                 </li>
@@ -119,6 +116,7 @@
             </ul>
         </div>
         <div id="formula-3-housing-loan" class="ps-criteria-detail__content">
+            <h5>Housing loan</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Housing Loan Tab Section.
                 </li>
@@ -129,6 +127,7 @@
             </ul>
         </div>
         <div id="formula-3-education-loan" class="ps-criteria-detail__content">
+            <h5>Education Loan </h5>
             <ul class="ps-list--arrow-circle">
                 <li>Education Loan Tab Section.
                 </li>
@@ -139,6 +138,7 @@
             </ul>
         </div>
         <div id="formula-3-insurance" class="ps-criteria-detail__content">
+            <h5>Insurance</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Insurance Tab Section.
                 </li>
@@ -149,6 +149,7 @@
             </ul>
         </div>
         <div id="formula-3-unit-trust" class="ps-criteria-detail__content">
+            <h5>Unit Trust</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Unit Trust Tab Section.
                 </li>
--- resources/views/frontend/htmlLayouts/aioMoreDetailF4.blade.php
+++ resources/views/frontend/htmlLayouts/aioMoreDetailF4.blade.php
@@ -10,35 +10,30 @@
                     <div class="table-new">
                         <div class="ps-block--product-info">
                             <h5>Salary (Mandatory)</h5>
-
                             <div class="ps-block__content">
                                 <p>Credit your salary through giro</p>
                                 <a class="ps-block__more content-detail" data-formula="4" data-detail-id="formula-4-salary">SHOW DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Card Spent</h5>
-
                             <div class="ps-block__content">
                                 <p>Transaction made on credit card</p>
                                 <a class="ps-block__more content-detail" data-formula="4" data-detail-id="formula-4-spend">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Home Loan</h5>
-
                             <div class="ps-block__content">
                                 <p>Housing loan with DBS/POSB</p>
                                 <a class="ps-block__more content-detail" data-formula="4" data-detail-id="formula-4-loan">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Insurance</h5>
-
                             <div class="ps-block__content">
                                 <p>Life insurance regular premium polices</p>
                                 <a class="ps-block__more content-detail" data-formula="4" data-detail-id="formula-4-insurance">Show DETAILS</a></div>
                         </div>
                         <div class="ps-block--product-info">
                             <h5>Investment</h5>
-
                             <div class="ps-block__content">
                                 <p>Dividend Credit &/OR Unit Trust  &/OR online equity</p>
                                 <a class="ps-block__more content-detail" data-formula="4" data-detail-id="formula-4-investment">Show DETAILS</a></div>
@@ -48,6 +43,7 @@
             </div>
         </div>
         <div id="formula-4-salary" class="ps-criteria-detail__content">
+            <h5>Salary (Mandatory)</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Salary Tab Section
                 </li>
@@ -58,6 +54,7 @@
             </ul>
         </div>
         <div id="formula-4-spend" class="ps-criteria-detail__content">
+            <h5>Card Spent</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Card Spent Tab Section.
                 </li>
@@ -68,6 +65,7 @@
             </ul>
         </div>
         <div id="formula-4-loan" class="ps-criteria-detail__content">
+            <h5>Home Loan</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Home Loan Tab Section.
                 </li>
@@ -78,6 +76,7 @@
             </ul>
         </div>
         <div id="formula-4-insurance" class="ps-criteria-detail__content">
+            <h5>Insurance</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Insurance Tab Section.
                 </li>
@@ -88,6 +87,7 @@
             </ul>
         </div>
         <div id="formula-4-investment" class="ps-criteria-detail__content">
+            <h5>Investment</h5>
             <ul class="ps-list--arrow-circle">
                 <li>Investment Tab Section.
                 </li>
